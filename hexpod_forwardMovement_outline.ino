fc86b18ce66ebfbb7170e633e682640c631607bc
quinnc04 <cquinn@mymail.mines.edu>
2017-04-14 12:11:19 -0600

    Add files via upload
    This now includes servo control code from the servotor library. A few of the constants still need to be set to appropriate values though.

--- hexpod_forwardMovement_outline.ino
+++ hexpod_forwardMovement_outline.ino
@@ -5,58 +5,108 @@
  *  All code for controlling the servos has been omitted until we can test this 
  *  and figure out which way we'll do it. I am more familiar with servo.h, but
  *  it may be easier if we can use servotor.h
+ *  
+ *  Update 4/13/17:
+ *  I added servo control conde using the servotor library.
+ *  It should be ready to test forward movement using the serial monitor on 19200 baud.
+ *  Enter '2' to move forward, then '1' to stop. Hopefully it works.
+ *  --Or actually I probably need to change a lot of the constants before this is ready. 
+ *    A lot of them are just placeholders until I calculate reasonable values.
+ *    Specifically, the following constants need to be calculated and/or calibrated:
+ *      maxStep, minStep, stepIncrement, yLift, yStand, xF, xM, and maybe a few more.
+ *  
  */
 
-//We'll need to figure out reasonable values for all these consts:
-const float maxStep = 50; // this is max z distance foot can move forward
-const float minStep = -10; // this is max z distance foot can move backward
-const float stepIncrement = 1; // how far each foot moves every loop- lower = smoother but slower
-const float yLift = 20;
-const float yStand = 10;
-const float xF = 10;
-const float xM = 15;
+#include "Servotor32.h" // call the servotor32 Library
+Servotor32 hexy; // create a servotor32 object
 
-float footPos1 = 0; // position of front right foot
-float footPos2 = 0; // position of front left foot
-float z1;
-float z2;
+//We'll need to figure out reasonable values for all these consts:
+const short maxStep = 50; // this is max z distance foot can move forward
+const short minStep = -10; // this is max z distance foot can move backward
+const short stepIncrement = 1; // how far each foot moves every loop- lower = smoother but slower
+const short yLift = 10; // height of body relative to foot while foot is lifted.
+const short yStand = 20; // height of body relative to foot while foot is planted.
+const short xF = 10; // lateral distance between foot and body for front feet (also back), while striding
+const short xM = 15; // lateral distance between foot and body for middle feet, while striding
+
+short footPos1 = 0; // position of front right foot
+short footPos2 = 0; // position of front left foot
+short z1; // delta z of tripod 1
+short z2; // delta z of tripod 2
 
 boolean tripod1Lifted;
 boolean tripod2Lifted;
 
 const int tripod1 = 1;
 const int tripod2 = 2;
-const int FR = 1; const int MR = 2; const int BR = 3;
-const int FL = 4; const int ML = 5; const int BL = 6;
 
-const float START_POS_HIP = (0+90);
-const float START_POS_KNEE = (30+90);
-const float START_POS_ANKLE = (60+90);
+const short START_POS_HIP = (0+90);
+const short START_POS_KNEE = (30+90);
+const short START_POS_ANKLE = (60+90);
 
 // Leg measurements in mm:
-const float TIBIA = 52.0;
-const float FEMUR = 49.0;
-const float COXA = 26.0;
+const short TIBIA = 52.0;
+const short FEMUR = 49.0;
+const short COXA = 26.0;
 //Here's pi:
-const float pi = 3.14159265359;
+const short pi = 3.14159265359;
 
 // Initialize joint angle vectors:
-float FRjoints[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
-float MRjoints[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
-float BRjoints[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
-float FLjoints[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
-float MLjoints[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
-float BLjoints[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
+short servoAngles[] = {START_POS_HIP, START_POS_KNEE, START_POS_ANKLE,
+                       START_POS_HIP, START_POS_KNEE, START_POS_ANKLE,
+                       START_POS_HIP, START_POS_KNEE, START_POS_ANKLE,
+                       START_POS_HIP, START_POS_KNEE, START_POS_ANKLE,
+                       START_POS_HIP, START_POS_KNEE, START_POS_ANKLE,
+                       START_POS_HIP, START_POS_KNEE, START_POS_ANKLE};
+// pinOffset allows servos to be split bt the two sides of the board
+const int pinOffset = 0;
+const int SERVO_1 = 1 + pinOffset;
+const int SERVO_2 = 2 + pinOffset;
+const int SERVO_3 = 3 + pinOffset;
+const int SERVO_4 = 4 + pinOffset;
+const int SERVO_5 = 5 + pinOffset;
+const int SERVO_6 = 6 + pinOffset;
+const int SERVO_7 = 7 + pinOffset;
+const int SERVO_8 = 8 + pinOffset;
+const int SERVO_9 = 9 + pinOffset;
+const int SERVO_10 = 10 + pinOffset;
+const int SERVO_11 = 11 + pinOffset;
+const int SERVO_12 = 12 + pinOffset;
+const int SERVO_13 = 13 + pinOffset;
+const int SERVO_14 = 14 + pinOffset;
+const int SERVO_15 = 15 + pinOffset;
+const int SERVO_16 = 16 + pinOffset;
+const int SERVO_17 = 17 + pinOffset;
+const int SERVO_18 = 18 + pinOffset;
 
+// Or maybe we'll do it this way:
+// To get array element number: (leg name)*LEG + (joint name)
+//    ex: right front knee => RF*LEG + KNEE = 1*3 + -2 = 1
+// To get pin numnber: (leg name)*LEG + (joint name) + pinOffset
+//
+// We'll probably want to rearrange these according to the best way to hook up the wires,
+//  but we should be able to keep the same formula.
+const int RF = 1; const int RM = 2; const int RB = 3;
+const int LF = 4; const int LM = 5; const int LB = 6;
+const int LEG = 3; const int HIP = -3; const int KNEE = -2; const int ANKLE = -1;
+
+// for movement test function:
+boolean goForward = false;
+const int GO = 50; // enter '2' in serial mon
+const int STOP = 49; // enter '1' in serial mon
 
 void setup() {
-  // put your setup code here, to run once:
-
+  hexy.begin();
+  
+  Serial.begin(19200);
+  while(!Serial){}
+  
+  // serialMoveTest Instructions:
+  Serial.println("Enter '2' to start movement, '1' to stop it.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  serialMoveTest(); // Test function for forward movement.
 }
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
@@ -157,43 +207,50 @@ void powerLegs(int tripod){
 /////////////////////////////////////////////////////////////////
 void moveLegs(int tripod){
   if(tripod==tripod1){
-    changeAngles(FR,FRjoints);
-    changeAngles(ML,MLjoints);
-    changeAngles(BR,BRjoints);
+    changeAngles(RF);
+    changeAngles(LM);
+    changeAngles(RB);
   }
   else{
-    changeAngles(FL,FLjoints);
-    changeAngles(MR,MRjoints);
-    changeAngles(BL,BLjoints);
+    changeAngles(LF);
+    changeAngles(RM);
+    changeAngles(LB);
   }
 }
 /////////////////////////////////////////////////////////////////
-void solveLegs(float y,float z,int tripod){
+void solveLegs(short y,short z,int tripod){
   if(tripod==tripod1){
-    solveJoints(xF,y,z,FRjoints);
-    solveJoints(xM,y,z,MLjoints);
-    solveJoints(xF,y,z,BRjoints);
+    solveJoints(xF,y,z,RF);
+    solveJoints(xM,y,z,LM);
+    solveJoints(xF,y,z,RB);
   }
   else{
-    solveJoints(xF,y,z,FLjoints);
-    solveJoints(xM,y,z,MRjoints);
-    solveJoints(xF,y,z,BLjoints);
+    solveJoints(xF,y,z,LF);
+    solveJoints(xM,y,z,RM);
+    solveJoints(xF,y,z,LB);
   }
 }
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-void changeAngles(int leg, float jointArray[]){
+void changeAngles(int leg){
   // this will change the angles of all joints for the given leg to the angles in the given array.
+  int hip = legIndex(leg,HIP);
+  int knee = hip + 1;
+  int ankle = hip + 2;
+  hexy.changeServo(hip + pinOffset, a2ms(servoAngles[hip]));
+  hexy.changeServo(knee + pinOffset, a2ms(servoAngles[knee]));
+  hexy.changeServo(ankle + pinOffset, a2ms(servoAngles[ankle]));
 }
 
 ///////////////////////////////////////////////////////////////////
 // This will solve joint angles, given x, y, and z in mm, relative to the coxa-body joint.
 // Also pass the appropriate joint array: solveJoints( x, y , z, appropriateJointArray ).
 // I'll have it return the angles in degrees.
-void solveJoints(float x, float y, float z,  float jointArray[]){
+void solveJoints(short x, short y, short z,  int leg){
   // Declare vars:
-  float L, l, H, t, f, h, tc, alpha, beta, theta;
+  int firstJointIndex = legIndex(leg,HIP);
+  short L, l, H, t, f, h, tc, alpha, beta, theta;
   // Let's do trig:
   L = abs(sqrt(pow(x,2)+pow(z,2))); l = L - COXA;
   H = abs(sqrt(pow(l,2)+pow(y,2)));
@@ -206,7 +263,65 @@ void solveJoints(float x, float y, float z,  float jointArray[]){
   beta = pi - h;
   theta = atan( z / x );
   // Result in degrees:
-  jointArray[0] = theta * 180 / pi;
-  jointArray[1] = alpha * 180 / pi;
-  jointArray[2] = beta * 180 / pi;
+  servoAngles[firstJointIndex] = theta * 180 / pi;
+  servoAngles[firstJointIndex+1] = alpha * 180 / pi;
+  servoAngles[firstJointIndex+2] = beta * 180 / pi;
 }
+
+//////////////////////////////////////////////////
+// This converts an angle to ms for the servos
+short a2ms(short inAngle){
+  short MS = inAngle*1000/90+1500;
+  return MS;
+}
+
+/////////////////////////////////////////////////
+// This gets input from serial
+// '0' is 48, '1' is 49, '2' is 50, look up ascii tables for the rest.
+int getVal(){
+  int input;
+  if (Serial.available() > 0) {
+    input = Serial.read();
+    Serial.println(input);
+  }
+  else{
+    input = -1;
+  }
+  return(input);
+}
+
+////////////////////////////////////////////////////
+// This function is to test forward movement.
+// Enter '2' to move forward, '1' to stop.
+void serialMoveTest(){
+  int val = getVal();
+  if(val==GO){
+    goForward = true;
+    Serial.println("GO");
+  }
+  while(val==GO){
+    moveForward();
+    val = getVal();
+    if(val==STOP){
+      goForward = false;
+      Serial.println("STOP");
+    }
+  }
+}
+
+///////////////////////////////////////////////////////
+// This kills all servos.
+void servoRest(){
+  // kill all servos
+  for(int i=0; i<32; i++){
+    hexy.changeServo(i,-1);
+  }
+}
+
+//////////////////////////////////////////////////////
+// This outputs the correct array index for the given leg and joint.
+// leg constants might need to be altered based on the wiring of the hexy.
+int legIndex(int leg, int joint){
+  return (leg*LEG + joint);
+}
+
